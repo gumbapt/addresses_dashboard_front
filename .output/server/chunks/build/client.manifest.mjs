01d9f030abe2ff61177a35efbc24143bce74c83b
Pedro Nave <pedronave@gmail.com>
2025-10-28 23:06:09 +0000

    added output build

---
+++ .output/server/chunks/build/client.manifest.mjs
@@ -0,0 +1,20 @@
+const client_manifest = {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "/Users/pedronave/Documents/addresses_dashboard_front/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "/Users/pedronave/Documents/addresses_dashboard_front/node_modules/nuxt/dist/app/entry.js"
+  }
+};
+
+export { client_manifest as default };
